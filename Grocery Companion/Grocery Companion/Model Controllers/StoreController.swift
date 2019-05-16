47e5d411e5c4eb0beb1d4a73848b6c4efb7e6cab
Andrew Dhan <iam.andrewdhan@gmail.com>
2019-05-15 22:52:24 -0400

    Update storecontroller

--- Grocery Companion/Grocery Companion/Model Controllers/StoreController.swift
+++ Grocery Companion/Grocery Companion/Model Controllers/StoreController.swift
@@ -1,24 +1,28 @@
-////
-////  StoreController.swift
-////  Grocery Companion
-////
-////  Created by Andrew Dhan on 11/1/18.
-////  Copyright © 2018 Andrew Dhan. All rights reserved.
-////
 //
-//import Foundation
-//import MapKit
-////temporary enum before incorporating Yelp API
+//  StoreController.swift
+//  Grocery Companion
 //
-//enum StoreName: Int{
-//    case traderJoes = 0
-//    case wholeFoods = 1
-//}
+//  Created by Andrew Dhan on 11/1/18.
+//  Copyright © 2018 Andrew Dhan. All rights reserved.
 //
-//class StoreController{
-//    static let shared = StoreController()
-//    //MARK: - Methods
-//    func bestStoreToBuyItems(_ items: [GroceryItem]) -> [Store]{
+
+import Foundation
+import MapKit
+//temporary enum before incorporating Yelp API
+
+enum StoreName: Int{
+    case traderJoes = 0
+    case wholeFoods = 1
+}
+
+class StoreController{
+    init() {
+        stores = [Store(name: "Trader Joe's", latitude:37.790780, longitude:-122.418039, context: moc), Store(name: "Whole Foods", latitude:37.781130,longitude:-122.399671, context: moc)]
+    }
+    
+    //MARK: - Methods
+    
+    func bestStoreToBuyItems(_ items: [Item]) -> [Store]{
 //        guard !items.isEmpty else {return [Store]()}
 //
 //        //Creates an array of Stores by running cheapestPlaceToBuy on each item in items
@@ -34,31 +38,34 @@
 //        //Create an array of stores containing stores with the heighest frequency. All stores in array will have same frequency
 //        let highestFrequency = lowPriceTuples.first!.1
 //        return lowPriceTuples.compactMap{ $0.1 == highestFrequency ? $0.0 : nil}
-//    }
-//    
-//    func estimatedCostForGroceries(store: Store, items: [GroceryItem]) -> Double? {
-//
+        return [Store]()
+    }
+    
+    func estimatedCostForGroceries(store: Store, items: [Item]) -> Double? {
+
 //        return items.compactMap{$0.cheapestPriceForStore(store: store)}.reduce(0,+)
-//
-//
-//    }
-//
-//    func itemsAvailableFrom(items: [GroceryItem]){
-//
-//    }
-//
-//    func averageStoreCoordinate() ->CLLocationCoordinate2D?{
+        return 0.0
+
+    }
+
+    func itemsAvailableFrom(items: [Item]){
+
+    }
+
+    func averageStoreCoordinate() ->CLLocationCoordinate2D?{
 //        let stores = StoreController.stores
 //        guard !stores.isEmpty else {return nil}
 //        let avgLatitude =  stores.reduce(0) {$0 + $1.coordinate.latitude}/Double(stores.count)
 //        let avgLongitude = stores.reduce(0) {$0 + $1.coordinate.longitude}/Double(stores.count)
 //        return CLLocationCoordinate2D(latitude: avgLatitude, longitude: avgLongitude)
-//    }
-//
-//
-//
-//    //MARK: - Properties
-//    static let stores =
-//        [Store(name: "Trader Joe's", latitude:37.790780, longitude:-122.418039),
-//         Store(name: "Whole Foods", latitude:37.781130,longitude:-122.399671)]
-//}
+        return CLLocationCoordinate2D()
+    }
+
+
+
+    //MARK: - Properties
+    private let moc = CoreDataStack.shared.mainContext
+    static let shared = StoreController()
+    
+    let stores = [Store]()
+}
